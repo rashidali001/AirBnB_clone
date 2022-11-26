bc80b25792fd44f1cff2f4180774f141106d1f34
rashidali001 <rashidestonian@gmail.com>
2022-11-26 16:51:29 +0300

    Cmd

--- models/base_model.py
+++ models/base_model.py
@@ -32,6 +32,7 @@ base_model module
 
 from datetime import datetime
 import uuid
+import json
 
 
 class BaseModel():
@@ -39,12 +40,26 @@ class BaseModel():
     BaseModel class
     '''
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         '''Initializing object...'''
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if len(kwargs) != 0:
+            for key, value in kwargs.items():
+                # strptime(value, format) - to convert a string to datetime
+                if key == "__class__":
+                    continue
+
+                if key == "created_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+                if key == "updated_at":
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+
+                setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         '''String representation of object'''
